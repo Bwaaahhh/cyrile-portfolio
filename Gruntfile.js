19603629ddaee1a9c1a837fffdd7e926e65e2b94
Unknown <cyril-ix@live.fr>
2017-03-15 11:52:12 +0100

    Add Grunt for Sass + add scss

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        sass: {                              // Nom de la tâche
+            dist: {                            // Nom de la sous-tâche
+                options: {                       // Options
+                    style: 'expanded'
+                },
+                files: {                         // Liste des fichiers
+                    'src/styles.css': 'scss/style.scss'       // 'destination': 'source'
+                }
+            }
+        },
+
+        watch:{
+            dist:{
+                files : ['scss/*.scss'],
+                tasks: ['sass:dist'],
+                options:{ spawn: false}
+            }
+        }
+    });
+
+  // Import du package
+  //grunt.loadNpmTasks('grunt-contrib-sass')
+
+  // Redéfinition de la tâche `default` qui est la tâche lancée dès que vous lancez Grunt sans rien spécifier.
+  // Note : ici, nous définissons sass comme une tâche à lancer si on lance la tâche `default`.
+  grunt.registerTask('default', ['watch:dist']);
+};
