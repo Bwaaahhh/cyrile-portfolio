bd54d1719a921f0680e5723389ad394b7345fd8e
Unknown <cyril-ix@live.fr>
2017-04-06 15:13:46 +0200

    Update contact form

--- src/app/page/contact/contact.component.ts
+++ src/app/page/contact/contact.component.ts
@@ -1,24 +1,29 @@
 import {Component} from '@angular/core';
-import { FormGroup , FormControl } from '@angular/forms';
+import { FormGroup , FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'contact',
-  templateUrl: './contact.component.html'
+  templateUrl: './contact.component.html',
+  styles:['input.ng-invalid {border-left: 5px solid red;}',
+    'input.ng-valid {border-left:5px solid green;}',
+    'input.ng-pristine {border-left: 1px solid grey;}']
   // styleUrls: ['../../app.component.css']
 })
 export class ContactComponent {
 
     userForm = new FormGroup({
-        name: new FormControl(),
+        nom: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+        prenom: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
         email: new FormControl(),
+        telephone: new FormControl(null,[Validators.required, Validators.pattern('^[0-9]{10}$')]),
         adresse: new FormGroup({
             rue: new FormControl(),
             ville: new FormControl(),
-            cp: new FormControl(),
+            cp: new FormControl(null, Validators.pattern('^[1-9][0-9]{4}$')),
         })
     });
 
-    // onSubmit(value: any){
-    //     console.log(value);
-    // }
+    onSubmit(){
+        console.log(this.userForm.value);
+    }
 }
