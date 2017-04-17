88f037a57fa69a778dc95407f681d13d481b1bce
Unknown <cyril-ix@live.fr>
2017-04-17 15:22:27 +0200

    Send mail with data ok !

--- src/app/page/contact/contact.component.ts
+++ src/app/page/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup , FormControl, Validators } from '@angular/forms';
-import { Http }    from '@angular/http';
+import { RequestOptions, URLSearchParams, Headers , Http}    from '@angular/http';
+
 
 @Component({
   selector: 'contact',
@@ -9,22 +10,21 @@ import { Http }    from '@angular/http';
     'input.ng-valid {border-left:5px solid green;}',
     'input.ng-pristine {border-left: 1px solid grey;}',
     'textarea.ng-invalid {border-left: 5px solid red;}',
-      'textarea.ng-valid {border-left:5px solid green;}',
-      'textarea.ng-pristine {border-left: 1px solid grey;}']
+    'textarea.ng-valid {border-left:5px solid green;}',
+    'textarea.ng-pristine {border-left: 1px solid grey;}']
   // styleUrls: ['../../app.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent{
 
     name: string;
     email: string;
     message: string;
     endpoint : string;
 
-    http : Http;
 
 
 
-    constructor(http : Http) {
+    constructor( private http : Http) {
         this.http = http;
     }
 
@@ -37,29 +37,53 @@ export class ContactComponent implements OnInit {
     });
 
     onSubmit(){
-        console.log(this.userForm.value);
+
+//         let body = new URLSearchParams();
+//
+//         body.set('email', this.userForm.value.email);
+//         body.set('name', this.userForm.value.nom);
+//         body.set('message', this.userForm.value.message);
+//
+// console.log(body);
+//application/json   application/x-www-form-urlencoded
+
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        let options = new RequestOptions({ headers: headers });
+
+        this.endpoint = "http://cyrile.marmier.codeur.online/mail.php";
+
+        // this.http.post(this.endpoint, body, options)
+        //     .subscribe(
+        //         response => console.log(response)
+        //     )
+
         let postVars = {
-          email : this.email,
-          name : this.name,
-          message : this.message
+            nom : this.userForm.value.nom,
+            prenom : this.userForm.value.prenom,
+            telephone : this.userForm.value.telephone,
+            email : this.userForm.value.email,
+            message : this.userForm.value.message
         };
 
-        this.http.post(this.endpoint, postVars)
-        .subscribe(
-            response => console.log(response),
-            response => console.log(response)
-        )
+        this.http.post(this.endpoint, postVars, options )
+            .subscribe(
+                response => console.log(response)
+            )
+
+
     }
 
-    ngOnInit() {
-    //This data could really come from some inputs on the interface - but let's keep it simple.
-    this.email = "cy.etievant@gmail.com";
-    this.name = "Hayden Pierce";
-    this.message = "Hello, this is Hayden.";
 
-        //Start php via the built in server: $ php -S localhost:8000
-            this.endpoint = "http://cyrile.marmier.codeur.online/mail.php";
-    }
+    // ngOnInit() {
+    //     //This data could really come from some inputs on the interface - but let's keep it simple.
+    //     this.email = "cy.etievant@gmail.com";
+    //     this.name = "Hayden Pierce";
+    //     this.message = "Hello, this is Hayden.";
+    //     //Start php via the built in server: $ php -S localhost:8000
+    //         this.endpoint = "http://cyrile.marmier.codeur.online/mail.php";
+    // }
 
 
 }
--- src/app/page/contact/mail.php
+++ src/app/page/contact/mail.php
@@ -1,9 +1,9 @@
 <?php
-$recipient = 'cy.etievant@gmail.com';
-$subject = 'new message\n';
-$headers = "From: \n";
-$message = '$params->message\n';
-mail($recipient, 'mon sujet', 'coucou');
+// $recipient = 'cy.etievant@gmail.com';
+// $subject = 'new message\n';
+// $headers = "From: \n";
+// $message = '$params->message\n';
+// mail($recipient, 'mon sujet', 'lala');
 
 switch($_SERVER['REQUEST_METHOD']){
     case("OPTIONS"): //Allow preflighting to take place.
@@ -14,19 +14,22 @@ switch($_SERVER['REQUEST_METHOD']){
     case("POST"): //Send the email;
         header("Access-Control-Allow-Origin: *");
 
-        $json = file_get_contents('php://input');
-        $params = json_decode($json);
-        $email = $params->email;
-        $name = $params->name;
-        $message = '$params->message\n';
+        $params = json_decode(file_get_contents('php://input'), true );
+        $nom = $params["nom"];
+        $prenom = $params["prenom"];
+        $telephone = $params["telephone"];
+        $mail = $params["email"];
+        $messagee = $params["message"];
 
-        $recipient = 'cy.etievant@gmail.com\n';
-        $subject = 'new message\n';
-        $headers = "From: toto@toto.com\n";
-        if(mail($recipient, $subject, $message, $headers)){
-            echo'totooo';
-        }
-    //    mail($recipient, $subject, $message, $headers);
+
+    //    $name = $params->name;
+        $message =  'Tu as recu un message de '.$prenom.''.$nom.' qui te dis '.$messagee.'. Tu peux le joindre au '.$telephone.' ou alors ici '.$mail.'';
+
+        $recipient = 'cy.etievant@gmail.com';
+        $subject = 'Tu as un message depuis ton appli!';
+        $headers = "From: hello@hello";
+
+        mail($recipient, $subject, $message, $headers);
         break;
     default: //Reject any non POST or OPTIONS requests.
         header("Allow: POST", true, 405);
